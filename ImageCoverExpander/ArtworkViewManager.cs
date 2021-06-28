9ebbb2183699e748531e0cb7f30018afa8d7f133
PixelBoom <rithikbhatia12@gmail.com>
2021-06-28 00:56:43 -0400

    Full compatibility with DiTails and Downloader
    Explanation for fix:
    - BeatSaverDownloader: For BeatSaverDownloader compatibility, I had to add an event for the cover expander like the original mod did. I did not use BS Utils because you do not ever need to use it if you're using SiraUtil. Instead, I get the MainMenuViewController, and on did finish (so everytime Solo menu is loaded) the cover is expanded.
    - DiTails: For DiTails I did 2 things. Instead of using localScale I used sizeDelta so the hitboxes dont interfere with other buttons. I also had to use BSML to get ClickableImage (which is what DiTails uses). If it is a clickable image then we know DiTails is installed and we have to change an additional property (DefaultColors) so it is compatible.

--- ImageCoverExpander/ArtworkViewManager.cs
+++ ImageCoverExpander/ArtworkViewManager.cs
@@ -3,41 +3,58 @@ using Zenject;
 using HMUI;
 using UnityEngine;
 using IPA.Utilities;
+using BeatSaberMarkupLanguage.Components;
 
 namespace ImageCoverExpander
 {
-    public class ArtworkViewManager : IInitializable
+    public class ArtworkViewManager : IInitializable, IDisposable
     {
         private StandardLevelDetailViewController _standardLevelViewController;
+        private MainMenuViewController _mainMenuViewController;
 
-        private static readonly Vector3 modifiedScale = new Vector3(5.2f, 4.1f, 0f);
-        private static readonly Vector3 modifiedPositon = new Vector3(-35f, -56f, 0f);
+        private static readonly Vector3 modifiedSizeDelta = new Vector2(69, 58);
+        private static readonly Vector3 modifiedPositon = new Vector3(-34f, -56f, 0f);
         private static readonly float modifiedSkew = 0;
 
-        public ArtworkViewManager(StandardLevelDetailViewController standardLevelDetailViewController)
+        public ArtworkViewManager(StandardLevelDetailViewController standardLevelDetailViewController, MainMenuViewController mainMenuViewController)
         {
             _standardLevelViewController = standardLevelDetailViewController;
+            _mainMenuViewController = mainMenuViewController;
         }
 
         public void Initialize()
         {
-            var levelbar = _standardLevelViewController.transform.Find("LevelDetail").Find("LevelBarBig");
-            ArtworkModification(levelbar);
+            _mainMenuViewController.didFinishEvent += OnDidFinishEvent;
         }
 
-        private void ArtworkModification(Transform levelBarTranform)
+        public void Dispose()
         {
+            _mainMenuViewController.didFinishEvent -= OnDidFinishEvent;
+        }
+
+        private void OnDidFinishEvent(MainMenuViewController _, MainMenuViewController.MenuButton __)
+        {
+            var levelBarTranform = _standardLevelViewController.transform.Find("LevelDetail").Find("LevelBarBig");
             if (!levelBarTranform) { return; }
             Plugin.Log.Notice("Changing artwork for " + levelBarTranform.name);
             try
             {
-                var imageTransform = levelBarTranform.Find("SongArtwork");
-                imageTransform.localScale = modifiedScale;
+                var imageTransform = levelBarTranform.Find("SongArtwork").GetComponent<RectTransform>();
+                imageTransform.sizeDelta = modifiedSizeDelta;
                 imageTransform.localPosition = modifiedPositon;
                 imageTransform.SetAsFirstSibling();
+
                 var imageView = imageTransform.GetComponent<ImageView>();
                 imageView.color = new Color(0.5f, 0.5f, 0.5f, 1);
+                imageView.preserveAspect = false;
                 FieldAccessor<ImageView, float>.Set(ref imageView, "_skew", modifiedSkew);
+
+                // For DiTails
+                var clickableImage = imageTransform.GetComponent<ClickableImage>();
+                if (clickableImage != null)
+                {
+                    clickableImage.DefaultColor = new Color(0.5f, 0.5f, 0.5f, 1);
+                }
             }
             catch (Exception e)
             {
